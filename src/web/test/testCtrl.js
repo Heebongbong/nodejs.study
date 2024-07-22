851da8c4e5f2fd71433fd8658d0b268ea6b39593
LeeHeeYong <gmldyd031@gmail.com>
2024-07-22 23:39:04 +0900

    0722 home

--- src/web/test/testCtrl.js
+++ src/web/test/testCtrl.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const { test } = require('./testSvc')
+const { test, searchByHashtag, getMyPosts } = require('./testSvc')
 const router = express.Router();
 
 router.get('/', test)
+router.get('/search/posts', getMyPosts)
+router.get('/search/:hashtag', searchByHashtag)
 
 module.exports = router
--- src/web/test/testSvc.js
+++ src/web/test/testSvc.js
@@ -1,20 +1,36 @@
 const axios = require("axios");
 
-exports.test = async (req, res, next) => {
-    try {
-        if(!req.session.jwt){
-            const tokenResult = await axios.post('http://localhost:8001/v1/token', {
+const API_URL = process.env.API_URL + process.env.API_VERSION;
+
+const apiGet = async (req, obj) => {
+    try{
+        if (!req.session.jwt) {
+            const tokenResult = await axios.post(`${API_URL}/token`, {
                 clientSecret: process.env.API_SECRET
             })
-            if(tokenResult.data?.code === 200){
+            if (tokenResult.data?.code === 200) {
                 req.session.jwt = tokenResult.data.token
             }else{
-                return res.status(tokenResult.data?.code).json(tokenResult.data)
+                return tokenResult;
             }
         }
-        const result = await axios.get('http://localhost:8001/v1/test', {
-            headers: {authorization: req.session.jwt }
-        })
+        return await axios.get(`${API_URL}${obj.url}`, {headers: obj.headers});
+    }catch (e) {
+        console.error(e)
+        if(e.response?.status === 419){
+            delete req.session.jwt;
+            return apiGet(req, obj)
+        }
+        throw e.response;
+    }
+}
+
+exports.test = async (req, res, next) => {
+    try {
+        const result = await apiGet(req, {
+            url: '/test', headers: {authorization: req.session.jwt }
+        });
+        if(result.data?.code !== 200) return res.stats(result.data?.code).json(result.data)
         return res.json(result.data)
     } catch (e) {
         console.error(e)
@@ -22,5 +38,33 @@ exports.test = async (req, res, next) => {
             return res.json(e.response.data)
         }
         return next(e)
+    }
+}
+
+exports.getMyPosts = async (req, res, next) => {
+    try {
+        const result = await apiGet(req, {
+            url: '/posts/my',
+            headers: { authorization: req.session.jwt }
+        });
+        if(result.data?.code !== 200) return res.stats(result.data?.code).json(result.data)
+        res.json(result.data)
+    } catch (e) {
+        console.error(e)
+        next(e)
+    }
+}
+
+exports.searchByHashtag = async (req, res, next) => {
+    try{
+        const result = await apiGet(req, {
+            url: `/posts/hashtag/${req.params.hashtag}`,
+            headers: {authorization: req.session.jwt }
+        })
+        if(result.data?.code !== 200) return res.stats(result.data?.code).json(result.data)
+        res.json(result.data)
+    }catch (e) {
+        console.error(e)
+        next(e)
     }
 }
